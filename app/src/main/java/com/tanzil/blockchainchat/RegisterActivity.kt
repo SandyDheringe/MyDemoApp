9c79d0717aff7a55f89b19045432900308911635
Sandeep Dheringe <sandeep.dheringe90@gmail.com>
2019-02-07 22:27:50 +0530

    Initial commit

---
+++ app/src/main/java/com/tanzil/blockchainchat/RegisterActivity.kt
@@ -0,0 +1,27 @@
+package com.tanzil.blockchainchat
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class RegisterActivity : AppCompatActivity()
+{
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_register)
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+    }
+
+    override fun onPause() {
+        super.onPause()
+    }
+}
